80b0e037c580e922e11a1d00e621ce4fa289f720
Andrey <andreikrauchanka1994@gmail.com>
2024-11-13 13:28:50 +0100

    init project

---
+++ eslint.config.cjs
@@ -0,0 +1,36 @@
+module.exports = [
+  {
+    files: ['**/*.{ts,tsx}'],
+    ignores: ['dist', 'node_modules'],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        tsconfigRootDir: __dirname,
+        project: './tsconfig.json',
+      },
+      globals: {
+        ...require('globals').browser,
+        "__dirname": true
+      },
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      'react-hooks': require('eslint-plugin-react-hooks'),
+      'react-refresh': require('eslint-plugin-react-refresh'),
+      prettier: require('eslint-plugin-prettier'),
+    },
+    rules: {
+      ...require('@eslint/js').configs.recommended.rules,
+      ...require('eslint-plugin-react-hooks').configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    },
+  },
+];
